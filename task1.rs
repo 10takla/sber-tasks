b6ecbcbb0e9c98a7d228811c0ed5d48c11252722
10takla <letifovabakar98@gmail.com>
2024-03-14 02:17:41 +0300

    realisation with score options

--- task1.rs
+++ task1.rs
@@ -1,5 +1,5 @@
 use rand::Rng;
-use std::time::Instant;
+use std::{fmt::{Display, Formatter}, time::Instant};
 
 const TIMESTAMPS_COUNT: usize = 50000;
 
@@ -25,7 +25,12 @@ struct Stamp {
     offset: i32,
     score: Score,
 }
+
 impl Stamp {
+    fn into_score(&self) -> String {
+        format!("| home: {} away: {} |", self.score.home, self.score.away)
+    }
+
     fn generate_stamp(previous_value: Stamp) -> Stamp {
         let score_changed: bool = rand::thread_rng().gen_bool(1.0 - PROBABILITY_SCORE_CHANGED);
         let home_score_change: bool = rand::thread_rng().gen_bool(PROBABILITY_HOME_SCORE);
@@ -61,17 +66,57 @@ fn generate_game() -> Vec<Stamp> {
     stamps
 }
 
-fn get_score(game_stamps: &[Stamp], offset: i32) -> (i32, i32) {
+enum ScoreCase {
+    Exact(Stamp),
+    Nearest(Stamp, i32),
+    TooMatch(Stamp)
+}
+
+impl Display for ScoreCase {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let message = match self {
+            ScoreCase::Exact(stamp) => format!("Счет: {}", stamp.into_score()),
+            ScoreCase::Nearest(stamp, offset) => format!(
+                "Offset {} не был зафикисрован. Ближайший offset {} со счетом: {}",
+                offset,
+                stamp.offset,
+                stamp.into_score()
+            ),
+            ScoreCase::TooMatch(stamp) => format!("Игра была окончена на момент offset {}. Последний счет {}", stamp.offset, stamp.into_score()),
+        };
+        write!(f, "{message}")
+    }
+}
+
+fn get_score(game_stamps: &[Stamp], offset: i32) -> ScoreCase {
     // dbg!(&game_stamps[0..offset as usize]);
-    let (_, st) = game_stamps.into_iter().enumerate().find(|(i, stamp)| {
-        stamp.offset == offset || (stamp.offset < offset && offset < game_stamps[i + 1].offset)
-    }).unwrap();
-    (st.score.home, st.score.away)
+    let last = game_stamps.last().unwrap();
+    if offset > last.offset {
+        return ScoreCase::TooMatch(*last);
+    }
+
+    for (i, stamp) in game_stamps.into_iter().enumerate() {
+        if stamp.offset == offset {
+            return ScoreCase::Exact(*stamp);
+        }
+        if stamp.offset < offset && offset < game_stamps[i + 1].offset {
+            return ScoreCase::Nearest(*stamp, offset);
+        }
+    }
+    panic!()
 }
 
 #[test]
 fn task1() {
-    let start = Instant::now();
-    dbg!(get_score(&generate_game()[0..], 10));
-    dbg!(Instant::now() - start);
+    use ScoreCase::*;
+    assert!(
+        match get_score(&generate_game()[0..=100], 100 * OFFSET_MAX_STEP + 1) {
+            TooMatch(..) => true,
+            _ => false
+        }
+    );
+
+    // let start = Instant::now();
+    println!("{}", get_score(&generate_game()[0..], 100 * OFFSET_MAX_STEP + 1));
+    // dbg!(Instant::now() - start);
 }
